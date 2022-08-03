a9208a75100482f474a924813e4c4f68ae69006a
mohamedtarek427 <102771793+mohamedtarek427@users.noreply.github.com>
2022-08-02 22:29:03 -0700

    update files

---
+++ src/components/Row.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+
+const Row = ({title, fetchURL}) => {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        axios.get(fetchURL).then((response) => {
+            setMovies(response.data.results)
+        })
+    },[fetchURL])
+  return (
+    <div>
+        <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+        <div>
+            <div className="relative flex items-center">
+                <div id={'slider'}>
+                {movies.map((item, id) => (
+                    <div className="w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                    </div>
+                ))}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Row
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 function Home() {
   return (
     <div>
         <Main />
+        <Row title='Upcoming' fetchURL={requests.requestUpcoming} />
+        <Row title='popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
     </div>
   )
 }
